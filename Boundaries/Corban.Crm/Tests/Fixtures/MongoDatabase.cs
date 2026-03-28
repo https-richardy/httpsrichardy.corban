fc381c5998d7de1771666e7e8a808fbc62a97ac4
Richard Garcia <code.richardy@gmail.com>
2026-03-28 12:47:43 -0300

    feature: this commit introduces mongo database fixture for managing mongo database container lifecycle in tests

---
+++ Boundaries/Corban.Crm/Tests/Fixtures/MongoDatabase.cs
@@ -0,0 +1,56 @@
+namespace Corban.Crm.TestSuite.Fixtures;
+
+public sealed class MongoDatabaseFixture : IAsyncLifetime
+{
+    public string ConnectionString { get; private set; } = default!;
+    public string DatabaseName { get; private set; } = AppDomain.CurrentDomain.FriendlyName;
+
+    public IMongoDatabase Database { get; private set; } = default!;
+    public IMongoClient Client { get; private set; } = default!;
+
+    // build a container with the official mongo image
+    // see more: https://dotnet.testcontainers.org/
+    private readonly IContainer _container = new ContainerBuilder("mongo:latest")
+        .WithCleanUp(true)
+        .WithExposedPort(27017)
+        .WithPortBinding(0, 27017)
+        .WithEnvironment("MONGO_INITDB_ROOT_USERNAME", "admin")
+        .WithEnvironment("MONGO_INITDB_ROOT_PASSWORD", "admin")
+        .Build();
+
+    public async ValueTask DisposeAsync() => await _container.StopAsync();
+    public async ValueTask InitializeAsync()
+    {
+        await _container.StartAsync();
+
+        var hostPort = _container.GetMappedPublicPort(27017);
+        var url = new MongoUrlBuilder
+        {
+            Scheme = ConnectionStringScheme.MongoDB,
+            Server = new MongoServerAddress("localhost", hostPort),
+            Username = "admin",
+            Password = "admin",
+            AuthenticationSource = "admin",
+            DatabaseName = DatabaseName
+        };
+
+        // mongo database connection string format:
+        // https://www.mongodb.com/docs/manual/reference/connection-string/
+        ConnectionString = url.ToString();
+
+        Client = new MongoClient(ConnectionString);
+        Database = Client.GetDatabase(DatabaseName);
+    }
+
+    public async ValueTask CleanDatabaseAsync()
+    {
+        var collections = await Database
+            .ListCollectionNames()
+            .ToListAsync();
+
+        foreach (var collection in collections)
+        {
+            await Database.DropCollectionAsync(collection);
+        }
+    }
+}
