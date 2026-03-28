7c0a6506a96c77200b76a7bffbd1f6af47de1683
Richard Garcia <code.richardy@gmail.com>
2026-03-28 13:54:13 -0300

    feature: this commit introduces additional global usings for filtering and utilities in Usings.cs

--- Boundaries/Corban.Crm/Tests/Usings.cs
+++ Boundaries/Corban.Crm/Tests/Usings.cs
@@ -5,6 +5,9 @@ global using Corban.Crm.Domain.Filtering;
 global using Corban.Crm.TestSuite.Fixtures;
 global using Corban.Crm.Infrastructure.Persistence;
 
+global using HttpsRichardy.Internal.Essentials.Filtering;
+global using HttpsRichardy.Internal.Essentials.Utilities;
+
 global using DotNet.Testcontainers.Builders;
 global using DotNet.Testcontainers.Containers;
 
