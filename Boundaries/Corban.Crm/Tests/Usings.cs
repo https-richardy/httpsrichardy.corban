cbd1e53eedf1f5cca3a1863e3c84961f483b72f7
Richard Garcia <code.richardy@gmail.com>
2026-03-28 13:17:25 -0300

    feature: reorganize usings.cs by grouping related global usings for better readability

--- Boundaries/Corban.Crm/Tests/Usings.cs
+++ Boundaries/Corban.Crm/Tests/Usings.cs
@@ -1,5 +1,13 @@
+global using Corban.Crm.Domain.Repositories;
+global using Corban.Crm.Domain.Aggregates;
+global using Corban.Crm.Domain.Filtering;
+
+global using Corban.Crm.TestSuite.Fixtures;
+global using Corban.Crm.Infrastructure.Persistence;
+
 global using DotNet.Testcontainers.Builders;
 global using DotNet.Testcontainers.Containers;
 
 global using MongoDB.Driver;
 global using MongoDB.Driver.Core.Configuration;
+global using AutoFixture;
