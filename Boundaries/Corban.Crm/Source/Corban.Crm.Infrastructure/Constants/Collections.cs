a21e5dbcbf9e04c79dacd05c493ebbaef2fdc76b
Richard Garcia <code.richardy@gmail.com>
2026-03-28 12:25:55 -0300

    feature: this commit introduces collections and documents constants for leads and customers

---
+++ Boundaries/Corban.Crm/Source/Corban.Crm.Infrastructure/Constants/Collections.cs
@@ -0,0 +1,7 @@
+namespace Corban.Crm.Infrastructure.Constants;
+
+public static class Collections
+{
+    public const string Leads = "corban.crm.leads";
+    public const string Customers = "corban.crm.customers";
+}
---
+++ Boundaries/Corban.Crm/Source/Corban.Crm.Infrastructure/Constants/Documents.cs
@@ -0,0 +1,19 @@
+﻿namespace Corban.Crm.Infrastructure.Constants;
+
+public static class Documents
+{
+    public static class Lead
+    {
+        public const string Identifier = "_id";
+
+        public const string CustomerId = "CustomerId";
+        public const string PipelineId = "PipelineId";
+
+        public const string Name = "Name";
+        public const string PhoneNumber = "PhoneNumber";
+        public const string Document = "Document";
+
+        public const string Stage = "Stage";
+        public const string Metadata = "Metadata";
+    }
+}
