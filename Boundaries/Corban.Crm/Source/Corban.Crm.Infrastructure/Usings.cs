4b385e70a04fbccf8bf90abc1ee824068441dd74
Richard Garcia <code.richardy@gmail.com>
2026-03-28 12:24:54 -0300

    feature: this commit introduces necessary usings for persistence and domain aggregates in usings.cs

--- Boundaries/Corban.Crm/Source/Corban.Crm.Infrastructure/Usings.cs
+++ Boundaries/Corban.Crm/Source/Corban.Crm.Infrastructure/Usings.cs
@@ -1,2 +1,13 @@
-global using Microsoft.Extensions.Logging;
-global using HttpsRichardy.Internal.Essentials.Patterns;
+global using Corban.Crm.Domain.Aggregates;
+global using Corban.Crm.Domain.Filtering;
+global using Corban.Crm.Domain.Repositories;
+
+global using Corban.Crm.Infrastructure.Constants;
+global using Corban.Crm.Infrastructure.Pipelines;
+
+global using HttpsRichardy.Internal.Infrastructure.Persistence;
+global using HttpsRichardy.Internal.Infrastructure.Persistence.Pipelines;
+
+global using MongoDB.Driver;
+global using MongoDB.Bson;
+global using MongoDB.Bson.Serialization;
