37e8571053adf05c886a2909277a03ed6e429a47
Richard Garcia <code.richardy@gmail.com>
2026-03-28 14:09:34 -0300

    feature: this commit introduces database settings record for database configuration

--- Boundaries/Corban.Crm/Source/Corban.Crm.CrossCutting/Configurations/ISettings.cs
+++ Boundaries/Corban.Crm/Source/Corban.Crm.CrossCutting/Configurations/ISettings.cs
@@ -2,6 +2,7 @@ namespace Corban.Crm.CrossCutting.Configurations;
 
 public interface ISettings
 {
+    public DatabaseSettings Database { get; }
     public FederationSettings Federation { get; }
     public ObservabilitySettings Observability { get; }
 }
--- Boundaries/Corban.Crm/Source/Corban.Crm.CrossCutting/Configurations/Settings.cs
+++ Boundaries/Corban.Crm/Source/Corban.Crm.CrossCutting/Configurations/Settings.cs
@@ -2,6 +2,7 @@ namespace Corban.Crm.CrossCutting.Configurations;
 
 public sealed record Settings : ISettings
 {
+    public DatabaseSettings Database { get; init; } = default!;
     public FederationSettings Federation { get; init; } = default!;
     public ObservabilitySettings Observability { get; init; } = default!;
 }
