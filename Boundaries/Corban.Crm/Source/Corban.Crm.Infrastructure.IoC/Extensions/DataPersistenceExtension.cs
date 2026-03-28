81aeb063e7e81c242d231f216525499edcb858b0
Richard Garcia <code.richardy@gmail.com>
2026-03-28 13:59:40 -0300

    feature: implement data persistence extension for MongoDB integration

---
+++ Boundaries/Corban.Crm/Source/Corban.Crm.Infrastructure.IoC/Extensions/DataPersistenceExtension.cs
@@ -0,0 +1,19 @@
+﻿namespace Corban.Crm.Infrastructure.IoC.Extensions;
+
+public static class DataPersistenceExtension
+{
+    public static void AddDataPersistence(this IServiceCollection services, ISettings settings)
+    {
+        services.AddSingleton<IMongoDatabase>(provider =>
+        {
+            var mongoClient = new MongoClient(settings.Database.ConnectionString);
+            var database = mongoClient.GetDatabase(settings.Database.DatabaseName);
+
+            return database;
+        });
+
+        services.AddTransient<ICustomerRepository, CustomerRepository>();
+        services.AddTransient<ILeadRepository, LeadRepository>();
+        services.AddTransient<IPipelineRepository, PipelineRepository>();
+    }
+}
